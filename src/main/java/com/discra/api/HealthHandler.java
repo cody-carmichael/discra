072c9f0453314dc703b0a77a2b9ff901b136f4ee
Cody Carmichael <pdahs1@gmail.com>
2025-10-14 20:48:23 -0500

    HTTP API v2: Return APIGatewayV2HTTPResponse + hardened AdminPing

--- src/main/java/com/discra/api/HealthHandler.java
+++ src/main/java/com/discra/api/HealthHandler.java
@@ -2,16 +2,19 @@ package com.discra.api;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import java.util.Map;
-import java.util.HashMap;
 
-public class HealthHandler implements RequestHandler<Map<String,Object>, Map<String,Object>> {
+
+public class HealthHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     @Override
-    public Map<String,Object> handleRequest(Map<String,Object> input, Context context) {
-        Map<String,Object> resp = new HashMap<>();
-        resp.put("statusCode", 200);
-        resp.put("headers", Map.of("Content-Type","application/json"));
-        resp.put("body", "{\"ok\":true}");
-        return resp;
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        System.out.println("HealthHandler invoked"); // basic log
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(200)
+                .withBody("{\"ok\":true}")
+                .withHeaders(Map.of("Content-Type","application/json","Access-Control-Allow-Origin","*"))
+                .build();
     }
 }
--- src/main/java/com/discra/api/VersionHandler.java
+++ src/main/java/com/discra/api/VersionHandler.java
@@ -2,17 +2,20 @@ package com.discra.api;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import java.util.Map;
-import java.util.HashMap;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
-public class VersionHandler implements RequestHandler<Map<String,Object>, Map<String,Object>> {
+import java.util.Map;
+
+public class VersionHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     @Override
-    public Map<String,Object> handleRequest(Map<String,Object> input, Context context) {
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
         String version = System.getenv().getOrDefault("VERSION", "dev");
-        Map<String,Object> resp = new HashMap<>();
-        resp.put("statusCode", 200);
-        resp.put("headers", Map.of("Content-Type","application/json"));
-        resp.put("body", "{\"version\":\"" + version + "\"}");
-        return resp;
+        System.out.println("VersionHandler invoked, VERSION=" + version); // basic log
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(200)
+                .withBody("{\"version\":\"" + version + "\"}")
+                .withHeaders(Map.of("Content-Type","application/json","Access-Control-Allow-Origin","*"))
+                .build();
     }
 }
